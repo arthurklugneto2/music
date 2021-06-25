63a4ed775c25b7d8b4a0f93e7d68d5a8984e9323
Arthur Klug <Arthur.Klug@bs.nttdata.com>
2021-06-25 17:22:14 -0300

    data

--- src/Data.java
+++ src/Data.java
@@ -50,25 +50,66 @@ public class Data {
         chords.add(new Chord("maj7",new String[]{"1","3","5","7"},new String[]{"4","3","4"},
                 "X Major 7, XMaj7,XM7",new String[]{}, new String[]{}));
 
-        //Chords[5] = ["dim", "1,3b,5b", "3,3", "X diminished", "3,6", "6,3"];
-        //Chords[6] = ["dim7", "1,3b,5b,6", "3,3,3", "X diminished 7"];
-        //Chords[7] = ["7b5", "1,3,5b,7b", "4,2,4", "X dominant 7 flat fifth, X7-5"];
-        //Chords[8] = ["7#5", "1,3,5#,7", "4,4,2", "X dominant 7 sharp fifth, X7+5"];
-        //Chords[9] = ["m7b5", "1,3b,5b,7b", "3,3,4", "Xm(b7), Xminor7b5"];
-        //Chords[10] = ["7b9", "1,3,5,7b,9b", "4,3,3,3", "X7-9, X7(add b9)"];
-        //Chords[11] = ["6", "1,3,5,6", "4,3,2", "X Major 6, XM6, XMaj6", "3,2,3", "2,3,4"];
-        //Chords[12] = ["m6", "1,3b,5,6", "3,4,2", "X minor 6, Xmin6", "4,2,3", "2,3,3"];
-        //Chords[13] = ["6add9", "1,3,5,6,9", "4,3,2,5", "X Major 6 add 9, X6/9, X6(add9)"];
-        //Chords[14] = ["9", "1,3,5,7b,9", "4,3,3,4", "X dominant 9, Xdom9, X7(add9)"];
-        //Chords[15] = ["m9", "1,3b,5,7b,9", "3,4,3,4", "X minor 9, Xmin9, Cm7(add9)"];
-        //Chords[16] = ["maj9", "1,3,5,7,9", "4,3,4,3", "X Major, XM9, XMaj7(add9)"];
-        //Chords[17] = ["add9", "1,3,5,9", "4,3,7", "C added 9th"];
-        //Chords[18] = ["11", "1,3,5,7b,9,11", "4,3,3,4,3", "X dominant 11, X7(add11)"];
-        //Chords[19] = ["m11", "1,3b,5,7b,9,11", "3,4,3,4,3", "X minor 11, Xmin11"];
-        //Chords[20] = ["13", "1,3,5,7b,9b,11", "4,3,3,4,3,4", "X dominant 13, X7(add13)"];
-        //Chords[21] = ["sus2", "1,2,5", "2,5", "X suspended 2", "5,5", "5,2"];
-        //Chords[22] = ["sus4", "1,4,5", "5,2", "X suspended 4", "2,5", "5,5"];
-        //Chords[23] = ["aug", "1,3,5#", "4,4", "X augmented, X+, X+5, X(#5)", "4,4", "4,4"];
+        chords.add(new Chord("dim",new String[]{"1","3b","5b"},new String[]{"3","3"},
+                "X diminished",new String[]{"3,6"}, new String[]{"6,3"}));
+
+        chords.add(new Chord("dim7",new String[]{"1","3b","5b","6"},new String[]{"3","3","3"},
+                "X diminished 7",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("7b5",new String[]{"1","3","5b","7b"},new String[]{"4","2","4"},
+                "X dominant 7 flat fifth, X7-5",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("7#5",new String[]{"1","3","5#","7"},new String[]{"4","4","2"},
+                "X dominant 7 sharp fifth, X7+5",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("m7b5",new String[]{"1","3b","5b","7b"},new String[]{"3","3","4"},
+                "Xm(b7), Xminor7b5",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("7b9",new String[]{"1","3","5","7b","9b"},new String[]{"4","3","3","3"},
+                "X7-9, X7(add b9)",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("6",new String[]{"1","3","5","6"},new String[]{"4","3","2"},
+                "X Major 6, XM6, XMaj6",new String[]{"3","2","3"}, new String[]{"2","3","4"}));
+
+        chords.add(new Chord("m6",new String[]{"1","3b","5","6"},new String[]{"3","4","2"},
+                "X minor 6, Xmin6",new String[]{"4","2","3"}, new String[]{"2","3","3"}));
+
+        chords.add(new Chord("m6",new String[]{"1","3b","5","6"},new String[]{"3","4","2"},
+                "X minor 6, Xmin6",new String[]{"4","2","3"}, new String[]{"2","3","3"}));
+
+        chords.add(new Chord("6add9",new String[]{"1","3","5","6","9"},new String[]{"4","3","2","5"},
+                "X Major 6 add 9, X6/9, X6(add9)",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("9",new String[]{"1","3","5","7b","9"},new String[]{"4","3","3","4"},
+                "X dominant 9, Xdom9, X7(add9)",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("m9",new String[]{"1","3b","5","7b","9"},new String[]{"3","4","3","4"},
+                "X minor 9, Xmin9, Cm7(add9)",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("maj9",new String[]{"1","3","5","7","9"},new String[]{"4","3","4","3"},
+                "X Major, XM9, XMaj7(add9)",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("add9",new String[]{"1","3","5","9"},new String[]{"4","3","7"},
+                "C added 9th",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("11",new String[]{"1","3","5","7b","9","11"},new String[]{"4","3","3","4","3"},
+                "X dominant 11, X7(add11)",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("m11",new String[]{"1","3b","5","7b","9","11"},new String[]{"3","4","3","4","3"},
+                "X minor 11, Xmin11",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("13",new String[]{"1","3","5","7b","9b","11"},new String[]{"4","3","4","3","4"},
+                "X dominant 13, X7(add13)",new String[]{}, new String[]{}));
+
+        chords.add(new Chord("sus2",new String[]{"1","2","5"},new String[]{"2","5"},
+                "X suspended 2",new String[]{"5","5"}, new String[]{"5","2"}));
+
+        chords.add(new Chord("sus4",new String[]{"1","4","5"},new String[]{"5","2"},
+                "X suspended 2",new String[]{"2","5"}, new String[]{"5","5"}));
+
+        chords.add(new Chord("aug",new String[]{"1","3","5#"},new String[]{"4","4"},
+                "X augmented, X+, X+5, X(#5)",new String[]{"4","4"}, new String[]{"4","4"}));
+
     }
 
     public List<Note> getKeys() {
